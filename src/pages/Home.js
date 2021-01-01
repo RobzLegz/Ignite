1077530719fb68f0e5ec49cce937c96a78992384
Robzlegz <robzlegz@gmail.com>
2021-01-01 17:51:40 +0200

    display games from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import {motion} from "framer-motion";
+import { upcomingGamesURL } from '../api';
 
 function Home() {
 
@@ -8,13 +12,43 @@ function Home() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadGames());
-    });
+    }, [dispatch]);
+
+    const {popular, newGames, upcoming} = useSelector((state) => state.games);
+    
+
 
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <GameList>
+            <h2>upcoming games</h2>
+            <Games>
+                {upcoming.map((game) => (
+                    <Game
+                        name={game.name} 
+                        released={game.released} 
+                        id={game.id} 
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </Games>
+        </GameList>
     );
 };
+
+const GameList = styled(motion.div)`
+    padding: 0rem 5rem;
+    h2{
+        padding:5rem 0rem;
+    }
+`;
+
+const Games = styled(motion.div)`
+    min-height:80vh;
+    display:grid;
+    grid-template-columns: repeat(auto-fit,minmax(500px,1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
 
 export default Home;
