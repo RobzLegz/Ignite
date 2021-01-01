f3748478ff7d2412c1f9af87255d0720c986bc94
Robzlegz <robzlegz@gmail.com>
2021-01-01 22:30:10 +0200

    styling popup boxes

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -25,15 +25,16 @@ const GlobaslStyle = createGlobalStyle`
         font-size:3rem;
         font-family: 'Abril Fatface', cursive;
         font-weight:lighter;
+        color:#333;
     }
     h3{
         font-size:1.3rem;
         color:#333;
-        padding:1.5rem;
+        padding:1.5rem 0rem;
     }
     p{
         font-size:1.2rem;
-        line-height: 200%normal;
+        line-height: 200%;
         color:#696969;
     }
     a{
