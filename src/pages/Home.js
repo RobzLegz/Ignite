ea9421065f7a1b613c4cc994fea81486194d5e40
Robzlegz <robzlegz@gmail.com>
2021-01-01 18:04:54 +0200

    adding different types of games

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,9 +20,33 @@ function Home() {
 
     return (
         <GameList>
-            <h2>upcoming games</h2>
+            <h2>Popular Games</h2>
+            <Games>
+                {popular.map((game) => (
+                    <Game
+                        name={game.name} 
+                        released={game.released} 
+                        id={game.id} 
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </Games>
+            <h2>upcoming Games</h2>
             <Games>
                 {upcoming.map((game) => (
+                    <Game
+                        name={game.name} 
+                        released={game.released} 
+                        id={game.id} 
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </Games>
+            <h2>New Games</h2>
+            <Games>
+                {newGames.map((game) => (
                     <Game
                         name={game.name} 
                         released={game.released} 
