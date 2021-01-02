7c90749f03d5af1ee116b8185869f4fa537b8190
Robzlegz <robzlegz@gmail.com>
2021-01-02 11:59:49 +0200

    adding stars

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -10,6 +10,8 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 function GameDetail({pathId}) {
 
@@ -21,6 +23,19 @@ function GameDetail({pathId}) {
             document.body.style.overflow = "auto";
             history.push("/");
         }
+    }
+
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for(let i = 1; 1 <= 5; i++){
+            if(i <= rating){
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            }else{
+                stars.push(<img alt="star" key={i} src={starEmpty}></img>)
+            }
+        }
+        return stars;
     }
 
     const getPlatform = (platform) => {
@@ -51,6 +66,7 @@ function GameDetail({pathId}) {
                             <div className="rating">
                                 <motion.h3 layoutId={`title${pathId}`}>{game.name}</motion.h3>
                                 <p>Rating: {game.rating}</p>
+                                {getStars()}
                             </div>
                             <Info>
                                 <h3>Platforms</h3>
@@ -117,6 +133,11 @@ const Stats = styled(motion.div)`
     display:flex;
     align-items:center;
     justify-content:space-between;
+    img{
+        width:2rem;
+        height:2rem;
+        display:inline;
+    }
 `;
 
 const Info = styled(motion.div)`
